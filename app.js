4b96e4fc25c367f060b5bc1fd64a070f78dc8906
Nandini Kaushal <83837934+nan3037@users.noreply.github.com>
2022-02-05 04:40:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+$('#navbar').load('navbar.html');
+$('#footer').load('footer.html');
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+    );
+  });
+
+  $('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name});
+    console.log(devices);
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = '/';
+  });
+
+
